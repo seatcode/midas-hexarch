73be0a762f8b68a82519e8d9961dcdde394ee7e4
desko27 <desko27@gmail.com>
2022-03-08 18:39:02 +0100

    Use native spread operator instead of deepmerge

--- src/domain/config/index.ts
+++ src/domain/config/index.ts
@@ -1,16 +1,13 @@
 import type { Config } from './types'
 
-import merge from 'deepmerge'
 import baseConfig from './config'
 import devConfig from './config.development'
 import proConfig from './config.production'
 
 function makeConfigForEnvironment (): Config {
-  const objectsToMerge = import.meta.env.MODE === 'production'
-    ? [baseConfig, proConfig]
-    : [baseConfig, devConfig]
-
-  return merge.all<Config>(objectsToMerge)
+  return import.meta.env.MODE === 'production'
+    ? { ...baseConfig, ...proConfig }
+    : { ...baseConfig, ...devConfig }
 }
 
 export default makeConfigForEnvironment()
