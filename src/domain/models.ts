11d588e62344f43551d198afe6d61cbd7ade2115
desko27 <desko27@gmail.com>
2022-03-15 16:36:16 +0100

    Simplify domain models a bit

--- src/domain/models.ts
+++ src/domain/models.ts
@@ -1,30 +1,34 @@
 import { Config } from './config/models'
 
 /**
- * Re-export some core types for convinience
+ * Common
+ * ---
+ * Please do NOT export, these are intended for internal usage only
  */
-export type { Config }
-
-/**
- * Use Cases
- */
-export abstract class UseCase {
+abstract class WithConfig {
   protected readonly config: Config
 
   constructor ({ config }: {config: Config}) {
+    /**
+     * Config is always injected into any domain piece
+     */
     this.config = config
   }
+}
 
+/**
+ * Public
+ * ---
+ * Exportable models so domain pieces inherit from these
+ */
+export abstract class Repository extends WithConfig {}
+export abstract class Mapper extends WithConfig {}
+export abstract class Service extends WithConfig {
   abstract execute (...args: unknown[]): Promise<unknown>
 }
+export abstract class UseCase extends Service {}
 
 /**
- * Repositories
+ * Re-export some core models for convinience
  */
-export abstract class Repository {
-  protected readonly config: Config
-
-  constructor ({ config }: {config: Config}) {
-    this.config = config
-  }
-}
+export type { Config }
